1c8229b7414e7829f0163b90908de5f0d5011d5d
Chris Nevers <christophernevers96@gmail.com>
2018-11-10 14:57:48 -0500

    Upload postings to cloud

--- app/src/main/java/com/chrisnevers/textbooks/SellActivity.java
+++ app/src/main/java/com/chrisnevers/textbooks/SellActivity.java
@@ -12,12 +12,19 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.ThrowOnExtraProperties;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.annotation.Nullable;
 
 public class SellActivity extends AppCompatActivity {
 
@@ -32,72 +39,93 @@ public class SellActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
+
+        // Initialize layout views
         textbookView = findViewById(R.id.textbookName);
         authorView = findViewById(R.id.authorName);
         isbnView = findViewById(R.id.isbn);
         conditionView = findViewById(R.id.condition);
         priceView = findViewById(R.id.price);
         submit = findViewById(R.id.sellBook);
+
+        // Set up firebase connection
         db = FirebaseFirestore.getInstance();
+
+        // Get current user
         FirebaseAuth auth = FirebaseAuth.getInstance();
         sellerEmail = auth.getCurrentUser().getEmail();
     }
 
     public void sellBook (View v) {
+
+        // Disable button to prevent multiple postings
         submit.setEnabled(false);
-        Toast.makeText(SellActivity.this,
-                "Checking if sold", Toast.LENGTH_SHORT).show();
-            String textbookName = textbookView.getText().toString();
-            String authorName = authorView.getText().toString();
-            String isbn = isbnView.getText().toString();
-            Float condition = conditionView.getRating();
-            String price = priceView.getText().toString();
 
-            HashMap<String, Object> textbookFields = new HashMap<>();
+        // Get form data
+        final String textbookName = textbookView.getText().toString();
+        final String authorName = authorView.getText().toString();
+        final String isbn = isbnView.getText().toString();
+        final Float condition = conditionView.getRating();
+        final String price = priceView.getText().toString();
 
-            textbookFields.put("author", authorName);
-            textbookFields.put("name", textbookName);
-            Toast.makeText(SellActivity.this,
-                    "Made hashmap", Toast.LENGTH_SHORT).show();
+        // Construct record to put in textbook database
+        HashMap<String, Object> textbookFields = new HashMap<>();
+        textbookFields.put("author", authorName);
+        textbookFields.put("name", textbookName);
 
-            db.collection("textbooks").document(isbn)
-                .set(textbookFields)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(SellActivity.this,
-                            "Success: Uploaded to textbooks db", Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(SellActivity.this,
+        // Store textbook in database
+        db.collection("textbooks").document(isbn)
+        .set(textbookFields)
+        .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(SellActivity.this,
                             "Fail: Uploaded to textbooks db", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+                }
+            }
+        });
 
+        // Construct record to put in copies database
         HashMap<String, Object> copiesField = new HashMap<>();
-
         copiesField.put("condition", Float.toString(condition));
         copiesField.put("price", price);
         copiesField.put("seller", sellerEmail);
 
-        Toast.makeText(SellActivity.this,
-                "Made hashmap", Toast.LENGTH_SHORT).show();
-
+        // Store the individual copy in database
         db.collection("copies").document(isbn)
-            .collection("copies")
-            .add(copiesField)
-            .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentReference> task) {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(SellActivity.this,
-                                "Success: Uploaded to copies db", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(SellActivity.this,
-                                "Fail: Uploaded to copies db", Toast.LENGTH_LONG).show();
-                    }
+        .collection("copies")
+        .add(copiesField)
+        .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if (task.isSuccessful()) {
+
+                    // Store id of document we created
+                    String copyId = task.getResult().getId();
+
+                    // Construct record to store in user's postings database
+                    HashMap<String, Object> sellerField = new HashMap<>();
+                    sellerField.put("id", copyId);
+
+                    // Save the id of the individual copy to the seller's profile
+                    db.collection("postings").document(sellerEmail)
+                    .collection("postings").document(isbn)
+                    .collection("copies").add(sellerField)
+                    .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(SellActivity.this,
+                                        "Fail: Uploaded to postings db", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(SellActivity.this,
+                            "Fail: Uploaded to copies db", Toast.LENGTH_LONG).show();
                 }
-            });
+            }
+        });
     }
 }
