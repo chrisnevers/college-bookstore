7662f9fdad1f4ede221726d02cf035a5a74defbb
AlbaraMehene <Albara_Mehene@student.uml.edu>
2018-12-01 11:42:01 -0500

    This styling boy

--- app/src/main/java/com/chrisnevers/textbooks/BrowseActivity.java
+++ app/src/main/java/com/chrisnevers/textbooks/BrowseActivity.java
@@ -1,6 +1,8 @@
 package com.chrisnevers.textbooks;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +10,7 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,6 +21,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class BrowseActivity extends AppCompatActivity {
 
@@ -36,18 +43,29 @@ public class BrowseActivity extends AppCompatActivity {
                             for (int i = 0; i < 10; i++) {
                                 for (DocumentSnapshot document : task.getResult()) {
                                     LinearLayout bookL = new LinearLayout(getApplicationContext());
+                                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.FILL_PARENT);
+                                    params.setMargins(0,0,0,200);
+                                    bookL.setLayoutParams(params);
                                     bookL.setOrientation(LinearLayout.VERTICAL);
+
                                     final String isbn = document.getId();
-                                    TextView isbnTV = createTextView(isbn);
-                                    TextView authorTV = createTextView(document.getData().get("author").toString());
+                                    TextView isbnTV = createISPN(isbn);
+                                    TextView authorTV = createAuthorView(document.getData().get("author").toString());
                                     TextView nameTV = createTextView(document.getData().get("name").toString());
+
                                     Button btn = new Button(getApplicationContext());
                                     btn.setText("Buy Now");
                                     btn.setBackgroundColor(getResources().getColor(R.color.primaryComplement));
+                                    ViewGroup.LayoutParams params2 = new ViewGroup.LayoutParams(400, ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    btn.setLayoutParams(params2);
                                     setOnClick(btn, isbn);
-                                    bookL.addView(isbnTV);
-                                    bookL.addView(authorTV);
+
+                                    nameTV.setTextSize(25);
+                                    nameTV.setTypeface(Typeface.DEFAULT_BOLD);
+
                                     bookL.addView(nameTV);
+                                    bookL.addView(authorTV);
+                                    bookL.addView(isbnTV);
                                     bookL.addView(btn);
                                     wrapper.addView(bookL);
                                 }
@@ -91,10 +109,36 @@ public class BrowseActivity extends AppCompatActivity {
 
     protected TextView createTextView(String s) {
         TextView tv = new TextView(getApplicationContext());
-        tv.setText(s);
+        tv.append(s);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.FILL_PARENT);
         params.weight = 1.0f;
         params.gravity = Gravity.TOP;
+        params.setMargins(0,0,0,10);
+        tv.setLayoutParams(params);
+        return tv;
+    }
+
+    protected TextView createAuthorView(String s) {
+        TextView tv = new TextView(getApplicationContext());
+        String author = "Author:  ";
+        tv.setText(author);
+        tv.append(s);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.FILL_PARENT);
+        params.weight = 1.0f;
+        params.gravity = Gravity.TOP;
+        params.setMargins(0,0,0,10);
+        tv.setLayoutParams(params);
+        return tv;
+    }
+
+    protected TextView createISPN(String s) {
+        TextView tv = new TextView(getApplicationContext());
+        tv.setText("ISBN:   ");
+        tv.append(s);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.FILL_PARENT);
+        params.weight = 1.0f;
+        params.gravity = Gravity.TOP;
+        params.setMargins(0,0,0,10);
         tv.setLayoutParams(params);
         return tv;
     }
